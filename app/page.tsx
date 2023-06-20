af495ed323887d62dfabd7bda3d44adfd3d0fa92
Ali Siddique <alisiddique@Alis-MacBook-Air.local>
2023-06-20 19:42:13 +0100

    from new laptop

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
         >
           <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
+            Introducing Ali
           </p>
         </a>
         <h1
